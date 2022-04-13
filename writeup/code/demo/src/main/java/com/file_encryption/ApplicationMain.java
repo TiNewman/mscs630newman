5831c5e56acd708d7176d2051af5f59956ea2491
TiNewman <titan.newman1@marist.edu>
2022-04-13 19:59:50 -0400

    Adding more user input.

--- writeup/code/demo/src/main/java/com/file_encryption/ApplicationMain.java
+++ writeup/code/demo/src/main/java/com/file_encryption/ApplicationMain.java
@@ -7,55 +7,124 @@ import java.awt.Label;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.plaf.FileChooserUI;
  
 public class ApplicationMain extends JDialog {
 
+  
+  public static void main(final String[] args) {
+
+    new ApplicationMain();
+  }
+
   public ApplicationMain() {
 
-    //Create a frame
-    //Frame f = new Frame();
-    //f.setSize(500, 300);
-     
-    //Prepare font
-    //Font font = new Font( "Times New Roman", Font.PLAIN, 22 );
-     
-    //Write something
-    //Label label = new Label("Starting Title");
-    //label.setForeground(Color.RED);
-    //label.setFont(font);
-    //f.add(label);
+    // Take the user's input.
+    Object[] choices = {"Encrypt a file", "Decrypt a file"};
+    Object defaultChoice = choices[0];
 
+    int optionTaken = JOptionPane.showOptionDialog(this, 
+      "What would you like to do?",
+      "Decision Time",
+      JOptionPane.YES_NO_OPTION,
+      JOptionPane.QUESTION_MESSAGE,
+      null,
+      choices,
+      defaultChoice);
+
+    
+    // Get what file the user wants.
     JFileChooser fileChooser = new JFileChooser();
     fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
     int result = fileChooser.showOpenDialog(this);
 
     String filePath = "";
+    File fileToRead = new File("holderFile.txt");
 
     if (result == JFileChooser.APPROVE_OPTION) {
-
+      fileToRead = fileChooser.getSelectedFile();
       filePath = fileChooser.getSelectedFile().getAbsolutePath();
     }
 
-    System.out.println("Path+File: " + filePath);
- 
-    //Make visible
-    /*f.setVisible(true);
-    f.addWindowListener(new WindowAdapter() {
 
-      public void windowClosing(WindowEvent e) {
+    // Get the password from the user.
+    String password = "";
 
-        System.exit(0);
-      }
-    });
+    if (optionTaken == 0) { 
+
+      password = passwordIntake(true);
+      // Encrypt here
+    }
+    else {
+
+      password = passwordIntake(false);
+      // Decrypt here
+    }
+
+    
+    // Get everything inside the file.
+    ArrayList<String> textInsideFile = getFileInsides(fileToRead);
+
+    /*for (String singleLine : textInsideFile) {
+      
+      System.out.println(singleLine);
+    }
     */
 
+    closeApplication();
   }
 
-  public static void main(final String[] args) {
 
-    new ApplicationMain();
+
+  public void closeApplication() {
+   
+    System.exit(0);
   }
+
+  public static String passwordIntake(boolean isNewPassword) {
+
+    String password = "";
+
+    while (password.length() == 0) {
+
+      // Get password
+      password = JOptionPane.showInputDialog("Enter your password please (can't be null)?");
+    }
+
+    if (isNewPassword) {
+
+      JOptionPane.showMessageDialog(null, "Your Password is: '" + password + "'\n Don't forget it!");
+    }
+
+    return password;
+  }
+
+  public static ArrayList<String> getFileInsides(File fileToRead) {
+
+    ArrayList<String> textInsideFile = new ArrayList<String>();
+
+    try {
+
+      Scanner myReader = new Scanner(fileToRead);
+
+      while (myReader.hasNextLine()) {
+
+        textInsideFile.add(myReader.nextLine());
+      }
+
+      myReader.close();
+    }
+    catch (FileNotFoundException e) {}
+
+    return textInsideFile;
+  }
+
 }
